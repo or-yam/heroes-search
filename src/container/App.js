66c089038d4ed647f3eb6ace19e8cb6fcd718de4
Oryam <48219965+or-yam@users.noreply.github.com>
2021-08-20 18:04:35 +0300

    New style and files stracture

---
+++ src/container/App.js
@@ -0,0 +1,17 @@
+import styles from './App.module.css';
+
+function App() {
+  return (
+    <main>
+      <header>
+        <h1>COMICS SUPER-HEROES</h1>
+      </header>
+
+      <footer className="f3">
+        All data and images from <a href="https://akabab.github.io/superhero-api/">superhero-api.com</a>{' '}
+      </footer>
+    </main>
+  );
+}
+
+export default App;
