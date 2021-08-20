e2e02eec2e73b3a30adc09f6e1c5f580fea92a99
Oryam <48219965+or-yam@users.noreply.github.com>
2021-08-20 22:36:26 +0300

    Apply cards components

--- src/container/App.js
+++ src/container/App.js
@@ -1,16 +1,19 @@
+import CardsList from '../components/CardsList/CardsList';
 import styles from './App.module.css';
 
 function App() {
   return (
-    <main>
+    <div className={styles.appContainer}>
       <header>
         <h1>COMICS SUPER-HEROES</h1>
       </header>
-
+      <main>
+        <CardsList />
+      </main>
       <footer className="f3">
         All data and images from <a href="https://akabab.github.io/superhero-api/">superhero-api.com</a>{' '}
       </footer>
-    </main>
+    </div>
   );
 }
 
