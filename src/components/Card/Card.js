41bc9f805be139e4aae85033c97b5f589b79efab
Oryam <48219965+or-yam@users.noreply.github.com>
2021-08-20 22:35:53 +0300

    Create Card component

---
+++ src/components/Card/Card.js
@@ -0,0 +1,28 @@
+import PropTypes from 'prop-types';
+import styles from './Card.module.css';
+
+const DEFAULT_IMAGE =
+  'https://www.pinclipart.com/picdir/big/560-5604123_silhouette-clipart-superhero-silhouette-superhero-transparent-background-png.png';
+
+const Card = ({ name, publisher, image }) => (
+  <li className={styles.card}>
+    <img className={styles.cardImage} alt="hero profile" src={image} width="200" height="200" />
+    <div className={styles.cardContent}>
+      <h2>{name}</h2>
+      <h3>{publisher}</h3>
+    </div>
+  </li>
+);
+
+Card.propTypes = {
+  name: PropTypes.string.isRequired,
+  publisher: PropTypes.string,
+  image: PropTypes.string
+};
+
+Card.defaultProps = {
+  publisher: 'Unknown',
+  image: DEFAULT_IMAGE
+};
+
+export default Card;
