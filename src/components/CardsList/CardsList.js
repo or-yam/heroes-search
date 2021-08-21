3801121f38843469396bdbc57b110d90ed7f7379
Oryam <48219965+or-yam@users.noreply.github.com>
2021-08-21 21:48:26 +0300

    Add not found message

--- src/components/CardsList/CardsList.js
+++ src/components/CardsList/CardsList.js
@@ -2,13 +2,16 @@ import PropTypes from 'prop-types';
 import Card from '../Card/Card';
 import styles from './CardsList.module.css';
 
-const CardList = ({ heroes }) => (
-  <ul className={styles.list}>
-    {heroes.map(({ id, name, biography, images }) => (
-      <Card key={id} id={id} name={name} publisher={biography.publisher} image={images.md} />
-    ))}
-  </ul>
-);
+const CardList = ({ heroes }) =>
+  heroes.length > 0 ? (
+    <ul className={styles.list}>
+      {heroes.map(({ id, name, biography, images }) => (
+        <Card key={id} id={id} name={name} publisher={biography.publisher} image={images.md} />
+      ))}
+    </ul>
+  ) : (
+    <p className={styles.noHeroesMessage}>Couldn&apos;t find heroes</p>
+  );
 
 CardList.propTypes = {
   heroes: PropTypes.arrayOf(PropTypes.object)
